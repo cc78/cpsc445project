8730ac56cf8873a67b7ce8d70b99412ea02adec3
jenni <jenni.hantula@gmail.com>
2015-03-23 12:24:33 -0700

    [CompressedSuffixArrayBuilder] started fast suffix sort

--- src/cpsc445project/CompressedSuffixArrayBuilder.java
+++ src/cpsc445project/CompressedSuffixArrayBuilder.java
@@ -22,6 +22,75 @@ public class CompressedSuffixArrayBuilder {
 		// TODO
 	}
 
+	/*
+	 * Fast suffix sorting as per Larsson NJ, Sadakane K (2007). 'Faster suffix sorting.' Theor. Comp. Sc. 387:258–272.
+	 * Basic version (for now).
+	 */
+	private void fastSuffixSort(String text) {
+		// TODO
+		int[] suffixArray = new int[text.length() + 1];
+		int[] groups = new int[text.length() + 1];		// stores group numbers
+		int[] lengths = new int[text.length() + 1];		// stores lengths of unsorted groups and combined sorted groups
+
+		int h = 1; 		// length of prefix by which suffixes are sorted
+
+		/* first pass --- use heapsort to sort prefixes of length 1 (is there a better way?) */
+
+		TreeMap<String, Integer> sa = new TreeMap<String, Integer>();
+		sa.put("", text.length());
+
+		for (int i = 0; i < text.length(); i++) {
+			sa.put(text.substring(i, i + 1), i);
+		}
+
+		int i = 0;
+		for (String key : sa.navigableKeySet()) {
+			suffixArray[i] = sa.get(key);
+			i++;
+		}
+
+		/* initialize groups[] */
+		// FIXME is there a better way?
+		int first = 0;
+		int last;
+		while (first < suffixArray.length) {
+			last = first + 1;
+			char s = text.charAt(suffixArray[first]);
+			while (last < suffixArray.length && text.charAt(suffixArray[last]) == s) {
+				last++;
+			}
+			for (int j = first; j < last; j++) {
+				groups[j] = last - 1;
+			}
+			first = last;
+		}
+
+		/* initialize lengths[] */
+		// FIXME is there a better way?
+		first = 0;
+		int sorted = 1;
+		while (first < groups.length) {
+			last = first + 1;
+			if (groups[last] != groups[first]) {
+				sorted = 1;
+				while (groups[last] != groups[last - 1]) {  // while sorted, continue
+					last++;
+				}
+			} else {
+				sorted = -1;
+				while (groups[last] == groups[last - 1]) {  // while unsorted, continue
+					last++;
+				}
+			}
+			for (int j = first; j < last; j++) {
+				lengths[j] = sorted * (last - first);
+			}
+		}
+
+		// TODO: continue from part (3) of the basic algorithm
+
+	}
+
 	/* Relatively inefficient method for building CSA for one (short) text segment */
 	private CompressedSuffixArray buildLastCSASegment(String text) {
 
@@ -51,9 +120,10 @@ public class CompressedSuffixArrayBuilder {
 		return inverseSuffixArray;
 	}
 
+	// TODO: generalize to use prefixes of suffixes (with given length)?
 	private int[] buildSuffixArray(String text) {
 
-		int[] suffixArray = new int[text.length()];
+		int[] suffixArray = new int[text.length() + 1];
 		TreeMap<String, Integer> sa = new TreeMap<String, Integer>();
 
 		sa.put("", text.length());
