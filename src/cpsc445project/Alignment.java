9de5f805acb142543e839063754c9b2d0bcd5651
Christine <i2i8@cs.ubc.ca>
2015-04-01 11:19:51 -0700

    Refactored code a bit and added a stack to do depth-first-search
    Signed-off-by: Christine <i2i8@cs.ubc.ca>

--- src/cpsc445project/Alignment.java
+++ src/cpsc445project/Alignment.java
@@ -1,90 +1,142 @@
 package cpsc445project;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
 import cpsc445project.ListMatrix;
 
 public class Alignment {
 
 	public static void main(String[] args) {
 		BWTIndexBuilder builder = new SimpleBWTIndexBuilder();
-		BWTIndex bwt = builder.build("actg");
-		computeAlignment(bwt, "at");
+		List<Character> alphabet = new ArrayList<Character>();
+		alphabet.add('a');
+		alphabet.add('c');
+		alphabet.add('t');
+		alphabet.add('g');
+		BWTIndex bwt = builder.build("actg", alphabet);
+		
+		Alignment a = new Alignment(bwt, "at", alphabet);
+		a.computeAlignment();
 	}
+
 	
 	final static double negInf = Double.NEGATIVE_INFINITY;
 	
 	final static double d = 5; //g
 	final static double e = 5; //s
 	final static ScoringMatrix scores = new ScoringMatrix();
+	
+	BWTIndex bwt;
+	String pattern;
+	ListMatrix N;
+	ListMatrix N1;
+	ListMatrix N2;
+	ListMatrix N3;
+	List<Character> alphabet;
+	
+	public Alignment(BWTIndex bwt, String pattern, List<Character> alphabet) {
+		this.bwt = bwt;
+		this.pattern = pattern;
+		this.N1 = new ListMatrix();
+		this.N2 = new ListMatrix();
+		this.N3 = new ListMatrix();
+		this.N = new ListMatrix();
+	}	
 		
-	public static void computeAlignment(BWTIndex bwt, String pattern) {
+	public void computeAlignment() {
 
 		int n = bwt.size();
-		int m = pattern.length();
-		ListMatrix N1 = new ListMatrix();
-		ListMatrix N2 = new ListMatrix();
-		ListMatrix N3 = new ListMatrix();
-		ListMatrix N = new ListMatrix();
 
 		//Using 0 indexing, where 0 = first character in string
-		for (int j=-1; j<=m; j++) {
-			N.set(0,j, 0);
-		}
+						
+		int depth = 0;
 		
+		for (int j=-1; j<=pattern.length(); j++) {
+				N.set(0,j, 0);
+			}
+			
 		for (int i=0; i<=n; i++) {
-			N1.set(i,0,-(d + i * e));
-		}		
-				
+				N1.set(i,0,-(d + i * e));
+			}
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(0);
+		
+		while (!stack.empty()) {
+			int i = stack.pop();
+			//align with current prefix
+//			localAlignment(i);
+			boolean isUp = true;
+			for (Character c : alphabet) {
+				//given the SA range of the current node, push on the min SA of its children
+				//do edge check
+				if (true) {
+//					stack.push(c);
+					stack.push(1);
+					isUp = false;
+				}
+			}
+			if (isUp) {
+				depth = depth - 1;
+			} else {
+				depth = depth + 1;
+			}
+			
+		}
+	}
+	
+	private void localAlignment(Integer i){
 		double n1;
 		double n2;
 		double n3;
-		
-		for (int i=1; i<=n; i++) {
-			for (int j=1; j<=m; j++) {
-				
-			    //N1
-			    if (N.get(i-1, j-1) > 0) {
-			    	n1 = N.get(i-1, j-1) + scores.getScore(bwt.get(i-1),pattern.charAt(j-1));
-			    } else {
-			    	n1 = negInf;
-			    }
-			    if (n1 > 0) {
-			    	N1.set(i, j, n1);
-			    }
-			    
-			    
-			    //N2
-			    if (N2.get(i-1, j) > 0 && N.get(i-1, j) > 0) {
-			    	n2 = max( N2.get(i-1, j)-e, N.get(i-1, j)-(d+e) );
-			    } else if (N2.get(i-1, j) > 0) {
-			    	n2 = N2.get(i-1, j) - e;
-			    } else if (N.get(i-1, j) > 0) {
-			    	n2 = N.get(i-1, j) - (d+e);
-			    } else {
-			    	n2 = negInf;
-			    }
-			    if (n2 > 0) {
-			    	N2.set(i, j, n2);
-			    }			    
-			    
-			    //N3
-			    if (N3.get(i, j-1) > 0 && N.get(i, j-1) > 0) {
-			    	n3 = max( N3.get(i, j-1)-e, N.get(i, j-1)-(d+e) );
-			    } else if (N3.get(i, j-1) > 0) {
-			    	n3 = N3.get(i, j-1) - e;
-			    } else if (N.get(i, j-1) > 0) {
-			    	n3 = N.get(i, j-1) - (d+e);
-			    } else {
-			    	n3 = negInf;
-			    }
-			    if (n3 > 0) {
-			    	N3.set(i, j, n3);
-			    }			    
+		for (int j=1; j<=pattern.length(); j++) {
+			
+		    //N1
+		    if (N.get(i-1, j-1) > 0) {
+		    	n1 = N.get(i-1, j-1) + scores.getScore(bwt.get(i-1),pattern.charAt(j-1));
+		    } else {
+		    	n1 = negInf;
+		    }
+		    if (n1 > 0) {
+		    	N1.set(i, j, n1);
+		    }
+		    
+		    
+		    //N2
+		    if (N2.get(i-1, j) > 0 && N.get(i-1, j) > 0) {
+		    	n2 = max( N2.get(i-1, j)-e, N.get(i-1, j)-(d+e) );
+		    } else if (N2.get(i-1, j) > 0) {
+		    	n2 = N2.get(i-1, j) - e;
+		    } else if (N.get(i-1, j) > 0) {
+		    	n2 = N.get(i-1, j) - (d+e);
+		    } else {
+		    	n2 = negInf;
+		    }
+		    if (n2 > 0) {
+		    	N2.set(i, j, n2);
+		    }			    
+		    
+		    //N3
+		    if (N3.get(i, j-1) > 0 && N.get(i, j-1) > 0) {
+		    	n3 = max( N3.get(i, j-1)-e, N.get(i, j-1)-(d+e) );
+		    } else if (N3.get(i, j-1) > 0) {
+		    	n3 = N3.get(i, j-1) - e;
+		    } else if (N.get(i, j-1) > 0) {
+		    	n3 = N.get(i, j-1) - (d+e);
+		    } else {
+		    	n3 = negInf;
+		    }
+		    if (n3 > 0) {
+		    	N3.set(i, j, n3);
+		    }			    
 
-			    double bestval = max(N1.get(i,j), N2.get(i,j), N3.get(i,j));
-			    System.out.println(bestval);
-			    N.set(i, j, bestval);
-			}
-		}
+		    double bestval = max(N1.get(i,j), N2.get(i,j), N3.get(i,j));
+		    System.out.println(bestval);
+		    N.set(i, j, bestval);
+		}		
 	}
 	
 	private static double max(double... vals) {
